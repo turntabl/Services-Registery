7b00bee41e15e945658c76b2b688df8f81fb631a
idawud <ismaildawud96@gmail.com>
2020-02-20 16:12:12 +0000

    rm unused images

--- init.sh
+++ init.sh
@@ -31,12 +31,14 @@ if [ $count -gt 2 ]; then
     echo "Restarting services:...."
     sudo docker-compose down 
     sudo docker-compose pull 
+    sudo docker rmi $(docker images -a | grep '<none>' | awk '{print $3}')
     sudo docker-compose build --no-cache
     sudo docker-compose up -d
 else 
     echo "Starting all services:....."
     sudo docker-compose down 
     sudo docker-compose pull 
+    sudo docker rmi $(docker images -a | grep '<none>' | awk '{print $3}')
     sudo docker-compose build --no-cache
     sudo docker-compose up -d
 fi
